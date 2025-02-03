fb9b221ef3d1f7a2634be0a4cb0e3f41e76de84f
minginew <thsalsrl7531@gmail.com>
2025-02-03 22:17:55 +0900

    :lipstick: Design: Logo SVG 적용

--- frontend/src/pages/user/PortfolioPage.tsx
+++ frontend/src/pages/user/PortfolioPage.tsx
@@ -4,14 +4,21 @@ import Certificate from '../../components/portfolio/Certificate';
 import Education from '../../components/portfolio/Education';
 import Project from '../../components/portfolio/Project';
 import Skill from '../../components/portfolio/Skill';
+import Logo1 from '../../../public/images/Logo1.svg?react';
+import Logo2 from '../../../public/images/Logo2.svg?react';
 
 const PortfolioPage = () => {
   return (
-    <main className='h-auto w-full px-5 py-10'>
-      <div className='flex h-svh w-full items-center justify-center bg-gray-600'>
-        <span className='h-20 text-5xl'>MainPage</span>
+    <div className='h-auto w-full'>
+      <div className='font-museo flex h-svh w-full flex-col items-center py-15'>
+        <Logo1 className='svg-icon' />
+        <Logo2 className='svg-icon' />
+        <span className='absolute bottom-0 flex flex-col items-center'>
+          <text className='text-main-blue font-[300]'>Scroll Down</text>
+          <img className='bg-nones w-10 animate-bounce' alt='scroll_down_icon' src='/icons/scrolldown.png' />
+        </span>
       </div>
-      <section className='bg-red-300'>
+      <section className='bg-[#2c2c2c]'>
         <About />
       </section>
       <section className='bg-orange-300'>
@@ -29,7 +36,7 @@ const PortfolioPage = () => {
       <section className='bg-purple-300'>
         <Project />
       </section>
-    </main>
+    </div>
   );
 };
 export default PortfolioPage;
