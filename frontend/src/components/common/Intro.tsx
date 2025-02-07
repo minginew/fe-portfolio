dc2fffc694c03254be5bafeae18839b4e1a6e29c
minginew <thsalsrl7531@gmail.com>
2025-02-07 15:51:37 +0900

    :sparkles: Feat-fe: Boder Animation 구현

--- frontend/src/components/common/Intro.tsx
+++ frontend/src/components/common/Intro.tsx
@@ -5,9 +5,13 @@ import Logo2 from '../../assets/images/Logo2.svg?react';
 const Intro = () => {
   const introRef = useRef<HTMLDivElement>(null);
   return (
-    <div ref={introRef} className='border-main-blue flex h-full w-full flex-col items-center justify-center border'>
-      <Logo2 className='animation-svg-draw h-auto w-[55%]' />
-      <Logo1 className='animation-svg-fill w-[30%]' />
+    <div ref={introRef} className='relative flex h-full w-full items-center justify-center'>
+      <div className='animation-border'>
+        <span className='flex flex-col items-center justify-center'>
+          <Logo2 className='animation-svg-draw h-auto w-[65%]' />
+          <Logo1 className='animation-svg-fill w-[30%]' />
+        </span>
+      </div>
     </div>
   );
 };
