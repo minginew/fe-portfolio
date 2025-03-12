f3d42c1243f74dff57e5793b2fb769f95911b86f
minginew <thsalsrl7531@gmail.com>
2025-03-12 14:50:35 +0900

    feat: AdminProjectList 구현 (#34)

--- frontend/src/components/admin/AdminProjectList.tsx
+++ frontend/src/components/admin/AdminProjectList.tsx
@@ -1,4 +1,61 @@
+import { useNavigate } from 'react-router-dom';
+import { useGetProjectsQuery } from '../../redux/api/projectApi';
 const AdminProjectList = () => {
-  return <div>AdminProjectList</div>;
+  const { data } = useGetProjectsQuery();
+  const navigator = useNavigate();
+  const handleCreateProject = () => {
+    navigator('edit');
+  };
+
+  return (
+    <div className='flex w-full flex-col items-center'>
+      <div className='border-main-blue text-main-blue flex w-[90%] items-center justify-between border-b-4 px-2'>
+        <div className='px-2 py-4 text-4xl font-bold'>Projects</div>
+        <button
+          className='hover:bg-main-blue h-10 cursor-pointer rounded-lg bg-blue-400 px-3 text-white'
+          onClick={handleCreateProject}
+        >
+          <span>작성하기</span>
+        </button>
+      </div>
+      <div className='my-5 flex w-full flex-col gap-4 px-2'>
+        {data?.map((project) => (
+          <div
+            key={project.projectId}
+            className='flex w-full cursor-pointer flex-col rounded-lg bg-white p-5 drop-shadow-md'
+            onClick={() => {
+              navigator(`${project.projectId}`);
+            }}
+          >
+            <div className='mb-4 text-xl font-bold'>{project.title}</div>
+            <div className='mb-4 flex h-48 w-full items-center justify-center rounded-t-3xl bg-blue-200'>
+              <img alt='logo' className='h-full w-full rounded-t-3xl object-cover' src={project.thumbnail} />
+            </div>
+            <div className='text-main-gray-200 mb-4 flex gap-1'>
+              <span>{project.startDate}</span>
+              <span>~</span>
+              <span>{project.endDate}</span>
+            </div>
+            <div className='mb-4'>{project.summary}</div>
+            <div className='mb-3 flex gap-3'>
+              {project.techstack?.map((stack, index) => (
+                <div key={index} className='rounded-2xl bg-blue-100 px-3 py-1 text-xs'>
+                  {stack}
+                </div>
+              ))}
+            </div>
+            <div className='flex gap-3'>
+              {project.roles?.map((role, index) => (
+                <div key={index} className='rounded-2xl bg-blue-100 px-3 py-1 text-xs'>
+                  {role}
+                </div>
+              ))}
+            </div>
+            <div></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default AdminProjectList;
