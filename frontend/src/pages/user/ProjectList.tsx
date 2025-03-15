f2c332f151b9ddfed3cb3307ee870b80bdcfd5cc
minginew <thsalsrl7531@gmail.com>
2025-03-15 11:48:46 +0900

    feat: User Project page 구현(#36)

---
+++ frontend/src/pages/user/ProjectList.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+import { useGetProjectsQuery } from '../../redux/api/projectApi';
+const ProjectList = () => {
+  const { data } = useGetProjectsQuery();
+  const navigator = useNavigate();
+
+  return (
+    <div className='flex w-full flex-col items-center px-2'>
+      <div className='border-main-blue text-main-blue mb-5 flex h-28 w-full max-w-6xl items-center justify-between border-b-1 px-2'>
+        <div className='font-museo px-2 py-4 text-4xl font-bold'>PROJECT</div>
+      </div>
+      <div className='my-5 flex w-full max-w-6xl flex-col gap-4 px-2'>
+        {data?.map((project) => (
+          <div
+            key={project.projectId}
+            className='flex w-full cursor-pointer flex-col rounded-lg bg-white p-5 drop-shadow-md'
+            onClick={() => {
+              navigator(`${project.projectId}`);
+            }}
+          >
+            <div className='mb-4 text-xl font-bold'>{project.title}</div>
+            <div className='mb-4 flex h-48 w-full items-center justify-center rounded-t-3xl bg-blue-200'>
+              <img alt='logo' className='h-full w-full rounded-t-3xl object-cover' src={project.thumbnail} />
+            </div>
+            <div className='text-main-gray-200 mb-4 flex gap-1'>
+              <span>{project.startDate}</span>
+              <span>~</span>
+              <span>{project.endDate}</span>
+            </div>
+            <div className='mb-4'>{project.summary}</div>
+            <div className='mb-3 flex gap-3'>
+              {project.techstack?.map((stack, index) => (
+                <div key={index} className='rounded-2xl bg-blue-100 px-3 py-1 text-xs font-medium text-blue-600'>
+                  {stack}
+                </div>
+              ))}
+            </div>
+            <div className='flex gap-3'>
+              {project.roles?.map((role, index) => (
+                <div key={index} className='rounded-2xl bg-blue-100 px-3 py-1 text-xs font-medium text-blue-600'>
+                  {role}
+                </div>
+              ))}
+            </div>
+            <div></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
