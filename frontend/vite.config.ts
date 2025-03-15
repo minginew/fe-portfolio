e32ac654255a51c5c5527d5182562d4029809681
minginew <thsalsrl7531@gmail.com>
2025-03-15 11:41:44 +0900

    feat: 파일경로 변경, vite alias 경로설정(#36)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: [
+      {find: "@", replacement: path.resolve(__dirname, "src")},
+      {find: "@icons", replacement: path.resolve(__dirname, "src/assets/icons")},
+      {find: "@images", replacement: path.resolve(__dirname, "src/assets/images")},
+      {find: "@components", replacement: path.resolve(__dirname, "src/components")},
+      {find: "@pages", replacement: path.resolve(__dirname, "src/pages")},
+      {find: "@hooks", replacement: path.resolve(__dirname, "src/hooks")},
+      {find: "@styles", replacement: path.resolve(__dirname, "src/styles")},
+      {find: "@redux", replacement: path.resolve(__dirname, "src/redux")},
+    ]
+  }
 });
