4e6147aeaa871b69cf221eab2bb163b336bf7f84
minginew <thsalsrl7531@gmail.com>
2025-03-15 13:00:21 +0900

    refactor: 라우터 경로 수정, redirect 로직 추가(#36)

--- frontend/src/pages/admin/LoginPage.tsx
+++ frontend/src/pages/admin/LoginPage.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import Logo2 from '../../assets/images/Logo2.svg?react';
-import { useAddDispatch } from '../../redux/redux';
+import { useSignInMutation } from '../../redux/api/authApi';
+import { useNavigate } from 'react-router-dom';
+
 const LoginPage = () => {
-  const dispatch = useAddDispatch();
+  const [signIn, { isLoading }] = useSignInMutation();
+  const navigator = useNavigate();
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -13,7 +16,13 @@ const LoginPage = () => {
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleLogin = () => {};
+  const handleLogin = async () => {
+    const result = await signIn({ ...form });
+    if (result.error) {
+      return;
+    }
+    navigator('/admin');
+  };
 
   return (
     <div className='flex h-svh w-full flex-col items-center justify-center bg-[url(src/assets/images/background_white.jpg)] pb-10'>
@@ -22,22 +31,30 @@ const LoginPage = () => {
         <input
           className='border-main-gray-200 focus:outline-main-blue focus:border-main-blue bg-main-gray-100 h-12 w-full rounded-xl border-2 pl-4 focus:outline-1'
           type='email'
+          name='email'
           placeholder='Email'
           onChange={handleFormChange}
         />
         <input
           className='border-main-gray-200 focus:outline-main-blue focus:border-main-blue bg-main-gray-100 h-12 w-full rounded-xl border-2 pl-4 focus:outline-1'
           type='password'
+          name='password'
           placeholder='Password'
           onChange={handleFormChange}
         />
       </div>
-      <div
-        className='text-main-gray-100 bg-main-blue flex h-12 w-72 cursor-pointer items-center justify-center rounded-xl text-xl font-medium opacity-60'
-        onClick={handleLogin}
-      >
-        <span>Sign in</span>
-      </div>
+      {isLoading ? (
+        <div className='text-main-gray-100 bg-main-blue flex h-12 w-72 cursor-pointer items-center justify-center rounded-xl text-xl font-medium opacity-60'>
+          <span>Loaidng...</span>
+        </div>
+      ) : (
+        <div
+          className='text-main-gray-100 bg-main-blue flex h-12 w-72 cursor-pointer items-center justify-center rounded-xl text-xl font-medium opacity-60'
+          onClick={handleLogin}
+        >
+          <span>Sign in</span>
+        </div>
+      )}
     </div>
   );
 };
