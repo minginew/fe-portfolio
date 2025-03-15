0564cdd747f2e928901af01c0e1038cc9e0d081d
minginew <thsalsrl7531@gmail.com>
2025-03-15 13:01:24 +0900

    feat: Footer 생성(#36)

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='bg-main-gray-100 flex h-72 w-full items-end justify-center'>
+      <div className='my-4 text-xs text-gray-500'>© 2025 sonmingi. All rights reserved</div>
+    </footer>
+  );
+};
+
+export default Footer;
