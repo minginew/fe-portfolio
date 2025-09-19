f4ed68ea887f1e30cf4906801a4f2b890e5c16d4
minginew <thsalsrl7531@gmail.com>
2025-09-19 23:30:02 +0900

    style: 디자인/레이아웃 조정(#51)

--- frontend/src/components/portfolio/About.tsx
+++ frontend/src/components/portfolio/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
   const aboutRef = useRef<HTMLDivElement>(null);
   useFadeAnimation(aboutRef);
   return (
-    <article className='flex h-full w-full max-w-6xl flex-col items-center pt-25 pb-10 text-white'>
+    <article className='flex w-full max-w-6xl flex-col items-center pt-25 pb-10 text-white'>
       <div ref={aboutRef} className='mb-10 flex w-full flex-col items-center'>
         <div className='text-5xl font-[500]'>ABOUT</div>
         <div className='flex w-full flex-col items-center md:mb-15 md:flex-row md:justify-center md:gap-10'>
