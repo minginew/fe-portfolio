cc66eab35d42d35cd43abb25127d57983d7e7c66
minginew <thsalsrl7531@gmail.com>
2025-09-23 04:19:45 +0900

    feat: 초기 렌더링 개선-Code Splitting(#54)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,21 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    visualizer({
+      filename: 'report/bundle-stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
@@ -19,5 +30,43 @@ export default defineConfig({
       { find: '@redux', replacement: path.resolve(__dirname, 'src/redux') },
       { find: '@util', replacement: path.resolve(__dirname, 'src/util') },
     ],
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // React 관련
+          if (
+            id.includes('node_modules/react/') ||
+            id.includes('node_modules/react-dom/') ||
+            id.includes('node_modules/react-router-dom/')
+          ) {
+            return '@react-vendor';
+          }
+
+          // Supabase
+          if (id.includes('node_modules/@supabase/')) {
+            return '@supabase-vendor';
+          }
+
+          // Highlight.js & Lowlight
+          if (
+            id.includes('node_modules/highlight.js/') ||
+            id.includes('node_modules/lowlight/')
+          ) {
+            return '@highlight-vendor';
+          }
+
+          // Tiptap & Prosemirror → 묶어서 하나로
+          if (
+            id.includes('node_modules/@tiptap/') ||
+            id.includes('node_modules/prosemirror/')
+          ) {
+            return '@tiptap-bundle';
+          }
+        },
+      },
+    },
   },
 });
